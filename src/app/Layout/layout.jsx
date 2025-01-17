424fc5b0d6b6748625aec1ddb4ae094a4ebcf8fd
HamizAsghar <techsmart5649@gmail.com>
2025-01-17 19:14:09 +0500

    scrooll changes

--- src/app/Layout/layout.jsx
+++ src/app/Layout/layout.jsx
@@ -1,31 +1,71 @@
-"use client"
-import { usePathname } from 'next/navigation'
-import React from 'react'
-import { Footer } from '@/app/footer/page'
-import Sidebar from '@/app/sideBar/page'
+// "use client"
+// import { usePathname } from 'next/navigation'
+// import React from 'react'
+// import { Footer } from '@/app/footer/page'
+// import Sidebar from '@/app/sideBar/page'
 
+
+// const Layout = ({ children }) => {
+//     const Pathname = usePathname()
+//     return (
+//         <div>
+//             {Pathname.startsWith("/admin") ? (
+//                 <div className="flex h-screen">
+//                     <div className='h-screen'>
+//                         <Sidebar />
+//                     </div>
+//                     <div className="flex-1 overflow-auto">
+//                         {children}
+//                     </div>
+//                 </div>
+//             ) : (
+//                 <div>
+//                     <div>{children}</div>
+//                     <Footer />
+//                 </div>
+//             )}
+//         </div>
+//     )
+// }
+
+// export default Layout
+
+
+
+
+
+
+
+
+"use client";
+import { usePathname } from "next/navigation";
+import React from "react";
+import { Footer } from "@/app/footer/page";
+import Sidebar from "@/app/sideBar/page";
 
 const Layout = ({ children }) => {
-    const Pathname = usePathname()
+    const Pathname = usePathname();
+
     return (
-        <div>
+        <div className="flex h-screen">
             {Pathname.startsWith("/admin") ? (
-                <div className="flex h-screen">
-                    <div className='h-screen'>
-                        <Sidebar />
-                    </div>
+                <>
+                    {/* Sidebar remains fixed */}
+                    <Sidebar />
+
+                    {/* Main content scrollable */}
                     <div className="flex-1 overflow-auto">
                         {children}
                     </div>
-                </div>
+                </>
             ) : (
-                <div>
-                    <div>{children}</div>
+                <div className="flex flex-col min-h-screen">
+                    <div className="flex-1">{children}</div>
                     <Footer />
                 </div>
             )}
         </div>
-    )
-}
+    );
+};
 
-export default Layout
+export default Layout;
